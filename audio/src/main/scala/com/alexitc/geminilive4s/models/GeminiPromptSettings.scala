3e23b4e4c025ea875e163596c955419c19cce44d
Alexis Hernandez <alexis22229@gmail.com>
2025-08-29 18:20:48 +0200

    Create custom types for most Gemini arguments (#4)
    With this approach, we used types but we allow users to use their own.
    
    These models now have custom classes: Voice, Model, Language.

--- audio/src/main/scala/com/alexitc/geminilive4s/models/GeminiPromptSettings.scala
+++ audio/src/main/scala/com/alexitc/geminilive4s/models/GeminiPromptSettings.scala
@@ -2,7 +2,7 @@ package com.alexitc.geminilive4s.models
 
 case class GeminiPromptSettings(
     prompt: String,
-    language: String = "en-US", // like en-US, es-ES
-    voiceName: String = GeminiVoice.random,
-    model: String = GeminiModel.flashPreview
+    language: GeminiLanguage = GeminiLanguage.EnglishUS,
+    voiceName: GeminiVoice = GeminiVoice.random,
+    model: GeminiModel = GeminiModel.FlashPreview
 )
